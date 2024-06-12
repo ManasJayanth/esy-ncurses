ce6db775dcbc3b260bfda55796b8ac0109f00999
Manas Jayanth <3097018+ManasJayanth@users.noreply.github.com>
2024-06-12 19:57:27 +0530

    Adds test program

---
+++ esy-test/test.c
@@ -0,0 +1,47 @@
+/*
+
+  https://www.paulgriffiths.net/program/c/srcs/curhellosrc.html
+  CURHELLO.C
+  ==========
+  (c) Copyright Paul Griffiths 1999
+  Email: mail@paulgriffiths.net
+
+  "Hello, world!", ncurses style.
+
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <curses.h>
+
+
+int main(void) {
+
+    WINDOW * mainwin;
+
+    
+    /*  Initialize ncurses  */
+
+    if ( (mainwin = initscr()) == NULL ) {
+	fprintf(stderr, "Error initialising ncurses.\n");
+	exit(EXIT_FAILURE);
+    }
+
+
+    /*  Display "Hello, world!" in the centre of the
+	screen, call refresh() to show our changes, and
+	sleep() for a few seconds to get the full screen effect  */
+
+    mvaddstr(13, 33, "Hello, world!");
+    refresh();
+
+
+    /*  Clean up after ourselves  */
+
+    delwin(mainwin);
+    endwin();
+    refresh();
+
+    return EXIT_SUCCESS;
+}
